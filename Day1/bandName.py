4368d2ba768cd8e38d7729e68bd94f67f02a6afb
fazer1929 <abhishekagrawal8888@gmail.com>
2020-11-11 01:34:01 +0530

    Day2 Part1

---
+++ Day1/bandName.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator " )
+city = input("Which city did you grew up in?\n")
+pet = input("What was your pet's name\n")
+print("A possuble band name coude be " + city.capitalize() + " " + pet.capitalize())
---
+++ Day2/LifeInDays.py
@@ -0,0 +1,4 @@
+curr_age = int(input("Enter Your Current Age:\n"))
+goal_age = int(input("What's Your Goal Age:\n"))
+years_left = goal_age-curr_age
+print(f"You Have {years_left*12} Months, {years_left*52} Weeks, and {years_left * 365} Days Left In Your Life")
